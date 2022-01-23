a262d926c5f8caa6c403305b1c71634f4d4bf6f6
Rishad <rishad.yamnoor@gmail.com>
2022-01-23 15:56:46 +0530

    connecting to wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import './App.css';
 
 // Constants
 const GITHUB_LINK = "https://github.com/EvolOfThings/g4NFTs";
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const checkWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert('Make sure you have MetaMask installed!');
+        return;
+      } else {
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        if (accounts?.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  useEffect(() => {
+    checkWallet();
+  }, []);
+
+   const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -16,6 +59,11 @@ const App = () => {
               src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
               alt="Monty Python Gif"
             />
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}>
+              Connect Wallet
+            </button>
           </div>
         </div>
         <div className="footer-container">
